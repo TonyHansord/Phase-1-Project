0b05cbbccecf1cffcc54350e8240765186833268
Anthony Hansord <tonyhansorddev@gmail.com>
2022-12-28 20:58:30 +1100

    added ability to fecth all shows and filter by decade/year

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,2 +1,9 @@
 const showSearch = `https://api.tvmaze.com/search/shows`;
-const scheduleSearch = `https://api.tvmaze.com/schedule?country=AU&date=`;
+const scheduleSearch = `https://api.tvmaze.com/schedule?`;
+const showCountry = [
+  { countryCode: 'US', countryName: 'USA' },
+  { countryCode: 'AU', countryName: 'Australia' },
+  { countryCode: 'GB', countryName: 'UK' },
+];
+
+const showData = 'http://localhost:3000/shows/1';
--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,26 @@
 const resultsSection = document.getElementById('results-section');
 const searchTypeRadios = document.querySelectorAll('input[name="search-type"]');
+const seventiesBtn = document.getElementById('70s');
+const eightiesBtn = document.getElementById('80s');
+const ninetiesBtn = document.getElementById('90s');
+const allShows = [];
+
 document.addEventListener('DOMContentLoaded', () => {
+  fetchAllShows();
+
+  seventiesBtn.addEventListener('click', () => {
+    let filteredData = filterShows('197');
+    renderResults(filteredData);
+  });
+  eightiesBtn.addEventListener('click', () => {
+    let filteredData = filterShows('198');
+    renderResults(filteredData);
+  });
+  ninetiesBtn.addEventListener('click', () => {
+    let filteredData = filterShows('199');
+    renderResults(filteredData);
+  });
+
   renderSearchForm(searchTypeRadios[0].id);
   searchTypeRadios.forEach((radio) => {
     radio.addEventListener('change', (event) => {
@@ -9,11 +29,42 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
+function fetchAllShows() {
+  for (let i = 0; i < 265; i++) {
+    fetch(`https://api.tvmaze.com/shows?page=${i}`)
+      .then((res) => res.json())
+      .then((shows) => {
+        shows.forEach((show) => {
+          allShows.push(show);
+        });
+        console.log('done');
+      });
+  }
+}
+
+function filterShows(timeFrame) {
+  return allShows.filter((show) => {
+    if (show['premiered'] !== null) {
+      return show['premiered'].startsWith(timeFrame);
+    }
+  });
+}
+
+// console.log(filteredData);
+
 function renderSearchForm(searchType) {
   const formContainer = document.getElementById('form-div');
   formContainer.innerHTML = '';
   const form = document.createElement('form');
   const input = document.createElement('input');
+  const dropDown = document.createElement('select');
+  showCountry.forEach((country) => {
+    const option = document.createElement('option');
+    option.id = country.countryCode;
+    option.value = country.countryCode;
+    option.textContent = country.countryName;
+    dropDown.appendChild(option);
+  });
   const inputLabel = document.createElement('label');
   const submitBtn = document.createElement('input');
   const inputString = `${searchType}-input`;
@@ -29,25 +80,29 @@ function renderSearchForm(searchType) {
       input.min = '1950-01-01';
       input.max = '2050-12-31';
       inputLabel.textContent = 'Date';
+      dropDown.style.display = 'flex';
+      fetchSchedule(dropDown.value, input.value);
       break;
     case 'show-name':
       input.type = 'text';
       input.placeholder = 'Enter Show Name';
       inputLabel.textContent = 'Show Name';
+      dropDown.style.display = 'none';
       break;
   }
 
   submitBtn.type = 'submit';
   form.addEventListener('submit', (e) => {
     e.preventDefault();
+    console.log(e);
     const searchBox = document.getElementById(e.target[0].id);
+    const countrySel = document.querySelector('select');
     console.log(searchBox.value);
     searchType === 'schedule'
-      ? fetchSchedule(searchBox.value)
+      ? fetchSchedule(countrySel.value, searchBox.value)
       : fetchQuery(searchBox.value);
-    form.reset();
   });
-  form.append(inputLabel, input, submitBtn);
+  form.append(inputLabel, input, dropDown, submitBtn);
   formContainer.append(form);
 }
 //Get value of searchbox when submit button pressed
@@ -56,10 +111,17 @@ function renderSearchForm(searchType) {
 // eg:`https://api.tvmaze.com/search/shows?%q=${value of search box}'`;
 
 function fetchQuery(query) {
-  const searchQuery = `${showSearch}?&q=${query}`;
-  fetch(searchQuery)
-    .then((res) => res.json())
-    .then((results) => renderResults(results));
+  const searchResults = allShows.filter((show) => {
+    if (show['name'] !== null) {
+      return show['name'].startsWith(query);
+    }
+  });
+  renderResults(searchResults);
+
+  // const searchQuery = `${showSearch}?&q=${query}`;
+  // fetch(searchQuery)
+  //   .then((res) => res.json())
+  //   .then((results) => renderResults(results));
 }
 
 function fetchEpisodes(showID) {
@@ -68,10 +130,10 @@ function fetchEpisodes(showID) {
     .then((results) => displayEpisodes(results));
 }
 
-function fetchSchedule(date) {
-  fetch(scheduleSearch + date)
+function fetchSchedule(country, date) {
+  fetch(`${scheduleSearch}country=${country}&date=${date}`)
     .then((res) => res.json())
-    .then((schedule) => console.log(schedule));
+    .then((schedule) => displaySchedule(schedule));
 }
 
 //clear any results currently on the page
@@ -88,24 +150,27 @@ const clearResults = (element) => {
 
 function renderResults(data) {
   clearResults(resultsSection);
-  console.log(data);
-  // let filteredData = filterResults(data.results, 'AU');
-  // console.log(filteredData);
+
   for (let result of data) {
     //create an element for each result
+
+    const showInfo = 'show' in result ? result.show : result;
+
     let showContainer = document.createElement('div');
     showContainer.className = 'result-container';
     // add a click event listener to clear results and display show information instead
     showContainer.addEventListener('click', () => {
-      fetchEpisodes(result.show.id);
+      fetchEpisodes(showInfo.id);
       clearResults(resultsSection);
     });
+
+    console.log(showInfo);
     let title = document.createElement('h1');
-    title.textContent = result.show.name;
+    title.textContent = showInfo.name;
     let showImg = document.createElement('img');
-    result.show.image !== null
-      ? (showImg.src = result.show.image.medium)
-      : (showImg.src = '#');
+    showInfo.image !== null
+      ? (showImg.src = showInfo.image.medium)
+      : (showImg.style.display = 'none');
     showContainer.appendChild(title);
     showContainer.appendChild(showImg);
     resultsSection.appendChild(showContainer);
@@ -132,6 +197,25 @@ function displayEpisodes(data) {
     epContainer.appendChild(summary);
     resultsSection.appendChild(epContainer);
   }
+}
+
+function displaySchedule(data) {
+  console.log(data);
+  clearResults(resultsSection);
+  const scheduleTable = document.createElement('table');
+
+  data.forEach((result) => {
+    const tableRow = document.createElement('tr');
+    const timeslot = document.createElement('td');
+    timeslot.textContent = result.airtime;
+    const showTitle = document.createElement('td');
+    showTitle.textContent = result.show.name;
+    const showDescription = document.createElement('td');
+    showDescription.innerHTML = result.summary;
+    tableRow.append(timeslot, showTitle, showDescription);
+    scheduleTable.appendChild(tableRow);
+  });
+  resultsSection.appendChild(scheduleTable);
 }
 
 //callback function to render show information
